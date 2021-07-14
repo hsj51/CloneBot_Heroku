cb6108ee6101df465acdc98e6098ea810da93f2e
Hrutvik Jagtap <hrutvikjagtap51@gmail.com>
2021-07-14 14:46:47 +0530

    CloneBot: Add support for overriding clone command
    *  Bcz sync is better than copy for backups
    
    Signed-off-by: Hrutvik Jagtap <hrutvikjagtap51@gmail.com>

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ path_to_gclone = ./gclone
 telegram_token = $telegram_token
 user_ids = $user_ids
 group_ids = $group_ids
+clone_command = $clone_command
 gclone_para_override = $gclone_para_override" >> "telegram_gcloner/config.ini"
 npm install http-server -g
 http-server -p $PORT &
